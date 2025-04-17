c42813102b8e9ea31e45afc8bc3e9396f49faea4
GODAOS <jmskkr@gmail.com>
2025-04-17 15:27:24 +0900

    redis 확인

--- src/main/kotlin/org/example/terraform2_app/Terraform2AppApplication.kt
+++ src/main/kotlin/org/example/terraform2_app/Terraform2AppApplication.kt
@@ -2,8 +2,10 @@ package org.example.terraform2_app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class Terraform2AppApplication
 
 fun main(args: Array<String>) {
